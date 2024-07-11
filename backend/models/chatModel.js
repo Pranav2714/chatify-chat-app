4947bce1807024d33dc774efe29273ed4a5e098a
Pranav Mahamunkar <pranav.mahamunkar11@gmail.com>
2024-07-11 19:30:46 +0530

    done

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -21,10 +21,6 @@ const chatSchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
     },
-    groupAdmin: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
   },
   {
     timestamps: true,
